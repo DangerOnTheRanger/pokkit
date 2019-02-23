7af10994a01b8a6a7092ad2f5f17375b1264560a
Kermit Alexander II <kotuxguy@gmail.com>
2019-02-23 17:21:36 -0600

    Add initial working client Docker skeleton

--- client/pokkit/client/__main__.py
+++ client/pokkit/client/__main__.py
@@ -2,4 +2,5 @@ def main():
     print("Hello World!")
 
 
-main()
+if __name__ == "__main__":
+    main()
--- client/setup.py
+++ client/setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
-
-setup(name='Pokkit',
+setup(name='pokkit',
       version='0.1',
       description='',
       author='',
       url='https://github.com/DangerOnTheRanger/',
-      package_dir={'pokkit.client': 'lib'},
+      packages=find_namespace_packages(),
       entry_points={'console_scripts': [
           'pokkit = pokkit.client.__main__:main'
       ]},
