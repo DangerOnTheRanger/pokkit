49a10ce75ca661025ccc6f2eaa55ef4d0346bb3b
Samuel Grayson <sam@samgrayson.me>
2019-02-23 22:30:52 -0600

    Undo cloud build, but still have --cache-from

--- client/test.sh
+++ client/test.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 set -o pipefail -o noclobber -o errexit -o nounset -o xtrace
+cd $(dirname $0)
+
+gcloud auth configure-docker
 COMMIT_SHA=$(git rev-parse HEAD)
 PROJECT_ID=pokkit
-IMAGE=gcr.io/${PROJECT_ID}/pokkit-client:${COMMIT_SHA}
+GCR_IMAGE=gcr.io/${PROJECT_ID}/pokkit-client:${COMMIT_SHA}
+
+docker build \
+       --tag ${GCR_IMAGE} \
+       --cache-from ${GCR_IMAGE} \
+       .
+
 docker run \
        --cap-add SYS_ADMIN \
        --device /dev/fuse \
        --security-opt apparmor:unconfined \
        --rm \
-       ${IMAGE}
+       ${GCR_IMAGE}
+
+docker push ${GCR_IMAGE}
