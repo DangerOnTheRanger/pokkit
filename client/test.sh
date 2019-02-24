f0f0207bb020470c6644b4694a6d9140d6263ee8
Samuel Grayson <sam@samgrayson.me>
2019-02-23 22:06:32 -0600

    Write cloud bulid cycle

--- client/test.sh
+++ client/test.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 set -o pipefail -o noclobber -o errexit -o nounset -o xtrace
-docker build . --tag pokkit-client
-docker run --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined pokkit-client --rm-pokkit-client
+COMMIT_SHA=$(git rev-parse HEAD)
+PROJECT_ID=pokkit
+IMAGE=gcr.io/${PROJECT_ID}/pokkit-client:${COMMIT_SHA}
+docker run \
+       --cap-add SYS_ADMIN \
+       --device /dev/fuse \
+       --security-opt apparmor:unconfined \
+       --rm \
+       ${IMAGE}
