9a9bafbed136359cc7ae32559a3600dcfffd8a22
Kermit Alexander II <kotuxguy@gmail.com>
2019-02-23 20:49:10 -0600

    Fix merge accident

--- client/setup.py
+++ client/setup.py
@@ -2,21 +2,6 @@
 
 from setuptools import setup, find_namespace_packages
 
-<<<<<<< HEAD
-setup(name='pokkit',
-      version='0.1',
-      description='',
-      author='',
-      url='https://github.com/DangerOnTheRanger/',
-      packages=find_namespace_packages(),
-      entry_points={'console_scripts': [
-          'pokkit = pokkit.client.__main__:main'
-      ]},
-      install_requires=[
-          'fuse-python'
-          ],
-     )
-=======
 setup(
     name='pokkit',
     version='0.1',
@@ -31,4 +16,3 @@ setup(
         ],
     },
 )
->>>>>>> 899e9ffff3a0458e39f833734c4dce9fb2b6b553
