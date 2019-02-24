e69021a97d38c282fdf380ca5b9cc89b63166520
Kermit Alexander II <kotuxguy@gmail.com>
2019-02-23 21:30:53 -0600

    Add FUSE boilerplate to test.sh

--- client/test.sh
+++ client/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -o pipefail -o noclobber -o errexit -o nounset -o xtrace
 docker build . --tag pokkit-client
-docker run --name pokkit-client --rm pokkit-client
+docker run --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined pokkit-client --rm-pokkit-client
